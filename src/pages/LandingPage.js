0b89330162f6ce1097fd8f34fddc0215fa2c7d45
Zetzher <j.abasolo.agenjo@gmail.com>
2020-12-22 00:24:02 +0100

    quitadas animaciones para el rendimiento de la página

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -2,13 +2,10 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Contacto from "../components/Contacto";
 import Directo from "../components/Directo";
-import ReactAudioPlayer from "react-audio-player";
-import IntroAudio from "../assets/musica/efecto_sonido_neon.mp3";
 
 let LandingPage = () => {
   return (
     <>
-      <ReactAudioPlayer src={IntroAudio} autoPlay volume={0.3} />
       <div className="container-landing">
         <h1 className="title-landing">
           HABLAREMOS <br />
